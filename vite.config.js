defc7922169af82250e4e86c2deb1e27b51c7b2c
James Dillon <jamesdillon_@outlook.com>
2026-02-10 13:10:31 +0000

    changed url to /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "JamesDillonDev.github.io/", // Set to your repo name
+  base: "/", // Set to your repo name
 })
